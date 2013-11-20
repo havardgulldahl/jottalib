98ed69bb293f6ea015de048b6489129bfcfa58a5
Håvard Gulldahl <havard@gulldahl.no>
2013-11-21 00:54:27 +0100

    getting files

--- src/jottafs/__init__.py
+++ src/jottafs/__init__.py
@@ -17,31 +17,120 @@
 # 
 # Copyright 2011,2013 Håvard Gulldahl <havard@gulldahl.no>
 
+# metadata
+
+__author__ = 'havard@gulldahl.no'
+__version__ = '0.1'
 
 # importing stdlib
 import sys, os
 import urllib, logging, datetime
-
 
 # importing external dependencies (pip these, please!)
 import requests
 import lxml, lxml.objectify
 import dateutil, dateutil.parser
 
+# some setup
 JFS_ROOT='https://www.jotta.no/jfs/'
+logging.basicConfig(level=logging.INFO)
 
 class JFSError(Exception):
     pass
+
+class JFSFile(object):
+    'OO interface to a file, for convenient access. Type less, do more.'
+    def __init__(self, fileobject, mountPoint, jfsdevice): # fileobject from lxml.objectify
+        self.f = fileobject
+        self.mountPoint = mountPoint
+        self.device = jfsdevice
+        lxml.objectify.dump(fileobject)
+
+    def stream(self):
+        'get the file contents'
+        self.device._jfs.get('%s?mode=bin' % self.path)
+        """
+            * name = 'jottacloud.sync.pdfname'
+            * uuid = '37530f11-d55b-4f31-acf4-27854813cd34'
+            currentRevision = None [ObjectifiedElement]
+                number = 1 [IntElement]
+                state = 'COMPLETED' [StringElement]
+                created = '2010-11-19-T12:34:18Z' [StringElement]
+                modified = '2010-11-19-T12:34:18Z' [StringElement]
+                mime = 'image/jpeg' [StringElement]
+                mstyle = 'IMAGE_JPEG' [StringElement]
+                size = 125848 [IntElement]
+                md5 = 'a0dc8233169b238681c43f9981efe8e1' [StringElement]
+                updated = '2010-11-19-T12:34:28Z' [StringElement]
+        """
+
+    @property
+    def name(self):
+        return unicode(self.f.attrib['name'])
+
+    @property
+    def path(self):
+        return '/'.join( (unicode(self.device.name), unicode(self.mountPoint.name), self.name) )
+
+    @property
+    def revisionNumber(self):
+        return int(self.f.currentRevision.number)
+    
+    @property
+    def created(self):
+        return dateutil.parser.parse(str(self.f.currentRevision.created))
+
+    @property
+    def modified(self):
+        return dateutil.parser.parse(str(self.f.currentRevision.modified))
+
+    @property
+    def updated(self):
+        return dateutil.parser.parse(str(self.f.currentRevision.updated))
+
+    @property
+    def size(self):
+        return int(self.f.currentRevision.size)
+    
+    @property
+    def md5(self):
+        return str(self.f.currentRevision.md5)
+    
+    @property
+    def mime(self):
+        return unicode(self.f.currentRevision.mime)
+    
+    @property
+    def state(self):
+        return unicode(self.f.currentRevision.state)
+    
 
 class JFSDevice(object):
     'OO interface to a device, for convenient access. Type less, do more.'
     def __init__(self, deviceobject, jfs): # deviceobject from lxml.objectify
         self.dev = deviceobject
-        self.__jfs = jfs
+        self._jfs = jfs
+        self.mountPoints = {mp.name:mp for mp in self.contents().mountPoints.iterchildren()}
 
     def contents(self, path=None):
-        c = self.__jfs.get('/' + self.name + path or '/')
-        print c
+        if isinstance(path, lxml.objectify.ObjectifiedElement) and hasattr(path, 'name'):
+            # passed an object, use .'name' as path value
+            path = '/%s' % path.name
+        c = self._jfs.get('/%s%s' % (self.name, path or '/'))
+        return c
+
+    def files(self, mountPoint):
+        """Get an iterator of JFSFile() from the given mountPoint. 
+        
+        "mountPoint" may be either an actual mountPoint element from JFSDevice.mountPoints{} or its .name. """
+        if isinstance(mountPoint, basestring):
+            # shortcut: pass a mountpoint name
+            mountPoint = self.mountPoints[mountPoint]
+        try:
+            return [JFSFile(f, mountPoint, self) for f in self.contents(mountPoint).files.iterchildren()]
+        except AttributeError as err:
+            # no files at all 
+            return [x for x in []]
 
     @property
     def modified(self):
@@ -57,7 +146,7 @@ class JFSDevice(object):
 
     @property
     def size(self):
-        return int(self.dev.size, 10)
+        return int(self.dev.size)
 
     @property
     def sid(self):
@@ -72,11 +161,13 @@ class JFS(object):
         self.fs = self.get(self.root)
 
     def get(self, url):
-        ident = 'jottafs-a-fuse-filesystem/0.1'
+        headers  = {'User-Agent':'JottaFS %s (https://git.gitorious.org/jottafs/jottafs.git)' % (__version__, ),
+                    'From': __author__}
         if not url.startswith('http'):
             # relative url
             url = self.root + url
-        r = requests.get(url, auth=self.auth)
+        logging.info("getting url: %s" % url)
+        r = requests.get(url, headers=headers, auth=self.auth)
         if r.status_code in ( 500, ):
             raise JFSError(r.reason)
         return lxml.objectify.fromstring(r.content)
@@ -90,31 +181,35 @@ class JFS(object):
     @property
     def locked(self):
         'return bool'
-        return self.fs is not None and self.fs.locked or None
+        return self.fs is not None and bool(self.fs.locked) or None
 
     @property
     def read_locked(self):
         'return bool'
-        return self.fs is not None and self.fs['read-locked'] or None
+        return self.fs is not None and bool(self.fs['read-locked']) or None
 
     @property
     def write_locked(self):
         'return bool'
-        return self.fs is not None and self.fs['write-locked'] or None
+        return self.fs is not None and bool(self.fs['write-locked']) or None
 
     @property
     def capacity(self):
         'return int of storage capacity in bytes'
-        return self.fs is not None and self.fs.capacity or -1
+        return self.fs is not None and int(self.fs.capacity) or -1
 
     @property
     def usage(self):
         'return int of storage usage in bytes'
-        return self.fs is not None and self.fs.usage or -1
+        return self.fs is not None and int(self.fs.usage) or -1
 
 
 if __name__=='__main__':
     jfs = JFS(os.environ['JOTTACLOUD_USERNAME'], password=os.environ['JOTTACLOUD_PASSWORD'])
     from pprint import pprint
     print lxml.objectify.dump(jfs.fs)
+    x = list(jfs.devices)[2]
+    #print lxml.objectify.dump(x.contents(x.mountPoints['Sync']))
+    files = x.files('Sync')
+
 
