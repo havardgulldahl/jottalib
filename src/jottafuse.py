c93bcd9bfa4e914347ebfdd206c378958feb2b3d
Håvard Gulldahl <havard@gulldahl.no>
2013-11-24 00:23:21 +0100

    adding fuse interface

---
+++ src/jottafuse.py
@@ -0,0 +1,115 @@
+# -*- encoding: utf-8 -*-
+#
+# This file is part of jottafs.
+# 
+# jottafs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# jottafs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with jottafs.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Copyright 2011,2013 Håvard Gulldahl <havard@gulldahl.no>
+
+# metadata
+
+__author__ = 'havard@gulldahl.no'
+__version__ = '0.1'
+
+# importing stdlib
+import sys, os
+import urllib, logging, datetime
+from time import time
+import itertools
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+# import jotta
+import jottafs
+
+# import dependenceis (get them with pip!)
+from fuse import FUSE, Operations, LoggingMixIn
+
+class JottaFuse(LoggingMixIn, Operations):
+    '''
+    A simple filesystem for JottaCloud.
+
+    '''
+
+    def __init__(self, username, password, path='.'):
+        self.client = jotta.JFS(username, password)
+        self.root = path
+
+    def xx_create(self, path, mode):
+        f = self.sftp.open(path, 'w')
+        f.chmod(mode)
+        f.close()
+        return 0
+
+    def destroy(self, path):
+        #self.client.close()
+        pass
+
+    def getattr(self, path, fh=None):
+        f = self.client.get(path)
+        pw = pwd.getpwuid( os.getuid() )
+        return {
+                'st_atime': isinstance(f, jottafs.JFSFile) and w.updated or time(),
+                'st_gid': pw.pw_gid,
+                'st_mode': isinstance(f, jottafs.JFSFile) and (stat.S_IFREG | 0444)  or (stat.S_IFDIR | 0755), 
+                'st_mtime': isinstance(f, jottafs.JFSFile) and w.modified or time(),
+                'st_size': isinstance(f, jottafs.JFSFile) and f.size  or 0,
+                'st_uid': pw.pw_uid,
+                }
+
+    def xx_mkdir(self, path, mode):
+        return self.sftp.mkdir(path, mode)
+
+    def read(self, path, size, offset, fh):
+        f = StringIO(self.client.get(path).stream())
+        f.seek(offset, 0)
+        buf = f.read(size)
+        f.close()
+        return buf
+
+    def readdir(self, path, fh):
+        yield '.'
+        yield '..'
+        p = self.client.get(path)
+        for el in itertools.chain(p.folders(), p.files()):
+            yield el.encode('utf-8')
+
+    def xx_rename(self, old, new):
+        return self.sftp.rename(old, self.root + new)
+
+    def xx_rmdir(self, path):
+        return self.sftp.rmdir(path)
+
+    def xx_unlink(self, path):
+        return self.sftp.unlink(path)
+
+    def xx_write(self, path, data, offset, fh):
+        f = self.sftp.open(path, 'r+')
+        f.seek(offset, 0)
+        f.write(data)
+        f.close()
+        return len(data)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('usage: %s <mountpoint>' % sys.argv[0])
+        sys.exit(1)
+
+    fuse = FUSE(JottaFuse(username=os.environ['JOTTACLOUD_USERNAME'], password=os.environ['JOTTACLOUD_PASSWORD']), 
+                sys.argv[2], foreground=True, nothreads=True)
+
+
