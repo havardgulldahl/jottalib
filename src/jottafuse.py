8576e9fcaccb51b586f50e0bf1370ac341e93549
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2014-04-12 23:52:47 +0200

    blacklist a lot of files that some OSes always look for, we dont need to look them up all the time. a first stab at a statfs() implementation, for df and stat support

--- src/jottafuse.py
+++ src/jottafuse.py
@@ -40,6 +40,9 @@ from jottalib import JFS
 # import dependenceis (get them with pip!)
 from fuse import FUSE, Operations, LoggingMixIn # this is 'fusepy'
 
+class JottaFuseError(JFS.JFSError):
+    pass
+
 class JottaFuse(LoggingMixIn, Operations):
     '''
     A simple filesystem for JottaCloud.
@@ -49,6 +52,19 @@ class JottaFuse(LoggingMixIn, Operations):
     def __init__(self, username, password, path='.'):
         self.client = JFS.JFS(username, password)
         self.root = path
+
+    def _getpath(self, path):
+        "A wrapper of JFS.getObject(), with some tweaks that make sense in a file system."
+        BLACKLISTED_FILENAMES = ('.hidden', '._', '.DS_Store', '.Trash', '.Spotlight-', '.hotfiles-btree',
+                                 'lost+found', 'Backups.backupdb')
+        _basename = os.path.basename(path)
+        for bf in BLACKLISTED_FILENAMES:
+            if _basename.startswith(bf):
+                raise JottaFuseError('Blacklisted file, refusing to retrieve it')
+
+        return self.client.getObject(path)
+
+
 
     def xx_create(self, path, mode):
         f = self.sftp.open(path, 'w')
@@ -62,7 +78,7 @@ class JottaFuse(LoggingMixIn, Operations):
 
     def getattr(self, path, fh=None):
         try:
-            f = self.client.getObject(path)
+            f = self._getpath(path)
         except JFS.JFSError:
             raise OSError(errno.ENOENT, '')
         pw = pwd.getpwuid( os.getuid() )
@@ -80,7 +96,7 @@ class JottaFuse(LoggingMixIn, Operations):
 
     def read(self, path, size, offset, fh):
         try:
-            f = StringIO(self.client.getObject(path).read())
+            f = StringIO(self._getpath(path).read())
         except JFS.JFSError:
             raise OSError(errno.ENOENT, '')
         f.seek(offset, 0)
@@ -95,13 +111,52 @@ class JottaFuse(LoggingMixIn, Operations):
             for d in self.client.devices:
                 yield d.name
         else:
-            p = self.client.getObject(path)
+            p = self._getpath(path)
             if isinstance(p, JFS.JFSDevice):
                 for name in p.mountPoints.keys():
                     yield name
             else:    
                 for el in itertools.chain(p.folders(), p.files()):
                     yield el.name
+
+    def statfs(self, path):
+        "Return a statvfs(3) structure, for stat and df and friends"
+        # from fuse.py source code:
+        # 
+        # class c_statvfs(Structure):
+        # _fields_ = [
+        # ('f_bsize', c_ulong), # preferred size of file blocks, in bytes
+        # ('f_frsize', c_ulong), # fundamental size of file blcoks, in bytes
+        # ('f_blocks', c_fsblkcnt_t), # total number of blocks in the filesystem
+        # ('f_bfree', c_fsblkcnt_t), # number of free blocks
+        # ('f_bavail', c_fsblkcnt_t), # free blocks avail to non-superuser
+        # ('f_files', c_fsfilcnt_t), # total file nodes in file system
+        # ('f_ffree', c_fsfilcnt_t), # free file nodes in fs
+        # ('f_favail', c_fsfilcnt_t)] # 
+        #
+        # On Mac OS X f_bsize and f_frsize must be a power of 2
+        # (minimum 512).
+
+        _blocksize = 512
+        _usage = self.client.usage
+        _fs_size = self.client.capacity 
+        if _fs_size == -1: # unlimited
+            # Since backend is supposed to be unlimited, 
+            # always return a half-full filesystem, but at least 1 TB)
+            _fs_size = max(2 * _usage, 1024 ** 4)
+        _bfree = ( _fs_size - _usage ) // _blocksize
+        return {
+            'f_bsize': _blocksize, 
+            'f_frsize': _blocksize,
+            'f_blocks': _fs_size // _blocksize,
+            'f_bfree': _bfree,
+            'f_bavail': _bfree,
+            # 'f_files': c_fsfilcnt_t,
+            # 'f_ffree': c_fsfilcnt_t,
+            # 'f_favail': c_fsfilcnt_t
+
+        }
+
 
     def xx_rename(self, old, new):
         return self.sftp.rename(old, self.root + new)
