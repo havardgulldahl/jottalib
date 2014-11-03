ad4adb77bb1799038555b29deb73c85dbec32b65
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2014-11-03 22:12:14 +0100

    tagging and uploading docs as part of release process

--- release.sh
+++ release.sh
@@ -6,7 +6,12 @@ function err {
 	exit 1;
 }
 
-pandoc --from=markdown --to=rst --output=README.txt README.md || err "Pandoc failed";
-python setup.py sdist $1 || err "setup.py failed"
-rm README.txt
+source bin/activate || err "couldnt activate virtualenv";
+VERSION=$(python setup.py --version) || err "couldnt get version";
+python setup.py sdist upload || err "setup.py upload failed";
+git tag -a "$VERSION" -m "Version $VERSION release" || err "couldnt tag git tree";
+git push --tags || err "problems pushing tags to central repository";
+pdoc --overwrite --html-dir dist/docs/"$VERSION" --html src/jottalib || err "pdoc generating docs failed";
+python setup.py upload_docs --upload-dir dist/docs/"$VERSION"/jottalib  || err "couldnt upload docs to pypi";
 
+echo "Enjoy your fresh $VERSION release!"
