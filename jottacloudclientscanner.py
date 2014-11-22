ed470a2456ff9212b9265695bea405e97c7fe8fe
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2014-11-22 21:09:39 +0100

    adding argparse to the mix, for nice verbose cli action

--- jottacloudclientscanner.py
+++ jottacloudclientscanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""A service to parse through a file structure and pass the tree to jottacloudclient.jottacloud.compare.
+"""A service to sync a local file tree to jottacloud.
 
 Run by crontab at some interval.
 
@@ -22,27 +22,43 @@ Run by crontab at some interval.
 #
 # Copyright 2014 Håvard Gulldahl <havard@gulldahl.no>
 
-import optparse, os, sys, logging
-logging.basicConfig(level=logging.WARNING)
+import os, os.path, sys, logging, argparse
 
 from jottalib.JFS import JFS
-from jottacloudclient import jottacloud
+from jottacloudclient import jottacloud, __version__
 
 if __name__=='__main__':
+    def is_dir(path):
+        if not os.path.isdir(path):
+            raise argparse.ArgumentTypeError('%s is not a valid directory' % path)
+        return path
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--loglevel', type=int, help='Loglevel', default=logging.WARNING)
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('--dry-run', action='store_true',
+                        help="don't actually do any uploads or deletes, just show what would be done")
+    parser.add_argument('topdir', type=is_dir, help='Path to local dir that needs syncing')
+    parser.add_argument('jottapath', help='The path at JottaCloud where the tree shall be synced (must exist)')
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
+
     jfs = JFS(os.environ['JOTTACLOUD_USERNAME'], password=os.environ['JOTTACLOUD_PASSWORD'])
 
-    for onlylocal, onlyremote, bothplaces in jottacloud.compare(sys.argv[1], sys.argv[2], jfs):
+    for onlylocal, onlyremote, bothplaces in jottacloud.compare(args.topdir, args.jottapath, jfs):
         print onlylocal, onlyremote, bothplaces
         print "uploading %s onlylocal files" % len(onlylocal)
         for f in onlylocal:
             logging.debug("uploading new file: %s", f)
-            jottacloud.new(f.localpath, f.jottapath, jfs)
+            if not args.dry_run:
+                jottacloud.new(f.localpath, f.jottapath, jfs)
         print "deleting %s onlyremote files" % len(onlyremote)
         for f in onlyremote:
             logging.debug("deleting cloud file that has disappeared locally: %s", f)
-            jottacloud.delete(f.jottapath, jfs)
+            if not args.dry_run:
+                jottacloud.delete(f.jottapath, jfs)
         print "comparing %s bothplaces files" % len(bothplaces)
         for f in bothplaces:
             logging.debug("checking whether file contents has changed: %s", f)
-            jottacloud.replace_if_changed(f.localpath, f.jottapath, jfs)
+            if not args.dry_run:
+                jottacloud.replace_if_changed(f.localpath, f.jottapath, jfs)
 
