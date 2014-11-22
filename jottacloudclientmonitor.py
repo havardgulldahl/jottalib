0ae1287ab8a48f553c4c1c85430175b100f0510d
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2014-11-22 07:10:02 +0100

    more name change

--- jottacloudclientmonitor.py
+++ jottacloudclientmonitor.py
@@ -4,20 +4,20 @@
 
 """
 #
-# This file is part of jottabox.
+# This file is part of jottacloudclient.
 #
-# jottabox is free software: you can redistribute it and/or modify
+# jottacloudclient is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# jottabox is distributed in the hope that it will be useful,
+# jottacloudclient is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with jottabox.  If not, see <http://www.gnu.org/licenses/>.
+# along with jottacloudclient.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Copyright 2014 Håvard Gulldahl <havard@gulldahl.no>
 
