1a5b93e1ebec80ad5ac92df4a74fbb9f28c9312e
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2014-12-07 17:51:52 +0100

    nicer output and catch and continue on errors

--- jottacloudclientscanner.py
+++ jottacloudclientscanner.py
@@ -25,7 +25,6 @@ Run by crontab at some interval.
 import os, os.path, sys, logging, argparse, netrc
 
 from clint.textui import progress, puts, colored
-
 from jottalib.JFS import JFS
 from jottacloudclient import jottacloud, __version__
 
@@ -40,6 +39,8 @@ if __name__=='__main__':
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('--dry-run', action='store_true',
                         help="don't actually do any uploads or deletes, just show what would be done")
+    parser.add_argument('--no-unicode', action='store_true',
+                        help="don't use unicode output")
     parser.add_argument('topdir', type=is_dir, help='Path to local dir that needs syncing')
     parser.add_argument('jottapath', help='The path at JottaCloud where the tree shall be synced (must exist)')
     args = parser.parse_args()
@@ -55,25 +56,42 @@ if __name__=='__main__':
 
     jfs = JFS(username, password)
 
-    for dirpath, onlylocal, onlyremote, bothplaces in jottacloud.compare(args.topdir, args.jottapath, jfs):
-        #print onlylocal, onlyremote, bothplaces
-        puts(colored.green("Entering dir: %s" % dirpath))
-        if len(onlylocal):
-            puts("uploading %s new files" % len(onlylocal))
-            for f in progress.bar(onlylocal):
-                logging.debug("uploading new file: %s", f)
-                if not args.dry_run:
-                    jottacloud.new(f.localpath, f.jottapath, jfs)
-        if len(onlyremote):
-            puts(colored.red("deleting %s removed files" % len(onlyremote)))
-            for f in progress.bar(onlyremote):
-                logging.debug("deleting cloud file that has disappeared locally: %s", f)
-                if not args.dry_run:
-                    jottacloud.delete(f.jottapath, jfs)
-        if len(bothplaces):
-            puts("comparing %s existing files" % len(bothplaces))
-            for f in progress.bar(bothplaces):
-                logging.debug("checking whether file contents has changed: %s", f)
-                if not args.dry_run:
-                    jottacloud.replace_if_changed(f.localpath, f.jottapath, jfs)
+    if not args.no_unicode: # use pretty characters to show progress
+        progress.BAR_EMPTY_CHAR=u'○'
+        progress.BAR_FILLED_CHAR=u'●'
+
+    errors = {}
+    def saferun(cmd, *args):
+        logging.debug('running %s with args %s', cmd, args)
+        try:
+            return apply(cmd, args)
+        except Exception as e:
+            puts(colored.red('Ouch. Something\'s wrong with "%s":' % args[0]))
+            logging.exception(e)
+            errors.update( {args[0]:e} )
+            return False
+
+    try:
+        for dirpath, onlylocal, onlyremote, bothplaces in jottacloud.compare(args.topdir, args.jottapath, jfs):
+            puts(colored.green("Entering dir: %s" % dirpath))
+            if len(onlylocal):
+                for f in progress.bar(onlylocal, label="uploading %s new files: " % len(onlylocal)):
+                    logging.debug("uploading new file: %s", f)
+                    if not args.dry_run:
+                        saferun(jottacloud.new, f.localpath, f.jottapath, jfs)
+            if len(onlyremote):
+                puts(colored.red("deleting %s removed files: " % len(onlyremote)))
+                for f in progress.bar(onlyremote):
+                    logging.debug("deleting cloud file that has disappeared locally: %s", f)
+                    if not args.dry_run:
+                        saferun(jottacloud.delete, f.jottapath, jfs)
+            if len(bothplaces):
+                for f in progress.bar(bothplaces, label="comparing %s existing files: " % len(bothplaces)):
+                    logging.debug("checking whether file contents has changed: %s", f)
+                    if not args.dry_run:
+                        saferun(jottacloud.replace_if_changed, f.localpath, f.jottapath, jfs)
+    except KeyboardInterrupt:
+        # Ctrl-c pressed, cleaning up
+        pass
+    puts('Finished, with %s errors' % len(errors))
 
